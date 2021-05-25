159945a1f8153007402ce84d6f8c74771fea31a5
Florent RODRIGUEZ <florent.rodriguez@neotys.com>
2021-05-25 15:51:57 +0200

    Improved generation script
    Change-Id: I6ea4ce0b76963888b76207f7f3fadf0052c23d5e

--- generateReleasePackage.sh
+++ generateReleasePackage.sh
@@ -2,23 +2,22 @@
 
 zip="/c/Program\ Files/7-Zip/7z.exe"
 
-# Package for Tosca >= 12.2
-rm -f Neoload_Add-on_Tosca12.2AndAbove.zip
+rm -f Neoload_Add-on_Tosca.zip
 
 cd installer
-eval $zip a ../Neoload_Add-on_Tosca12.2AndAbove.zip ./*
+eval $zip a ../Neoload_Add-on_Tosca.zip ./*
 cd ..
 
 cd NeoloadTBoxProxy/bin/Release
 mkdir -p TBox
 cp NeoloadTBoxProxy.dll TBox
 cp ../../../NeoLoadTBoxAddOn/bin/Release/ILMerge/NeoLoadTBoxAddOn.dll TBox
-eval $zip a ../../../Neoload_Add-on_Tosca12.2AndAbove.zip TBox/
+eval $zip a ../../../Neoload_Add-on_Tosca.zip TBox/
 rm -rf TBox
 cd ../../..
 
 cd NeoLoadToscaCommanderAddOn/bin/Release
 mkdir -p ToscaCommander
 cp NeoLoadToscaCommanderAddOn.dll ToscaCommander
-eval $zip a ../../../Neoload_Add-on_Tosca12.2AndAbove.zip ToscaCommander/
+eval $zip a ../../../Neoload_Add-on_Tosca.zip ToscaCommander/
 rm -rf ToscaCommander
