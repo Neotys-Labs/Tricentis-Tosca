a961560350c4f5b3bdaf7f1848224832af2c37e2
Franz Gasser <f.gasser@tricentis.com>
2021-06-05 20:12:12 +0800

    Added support for DataExchangeApi

---
+++ NeoLoadTBoxAddOn/client/NeoLoadDataExchangeApiInstance.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Neotys.DataExchangeAPI.Client;
+using Neotys.DataExchangeAPI.Error;
+using Neotys.DataExchangeAPI.Model;
+
+namespace NeoLoadAddOn.client
+{
+    public class NeoLoadDataExchangeApiInstance
+    {
+        private static NeoLoadDataExchangeApiInstance _instance = null;
+        private IDataExchangeAPIClient _client;
+
+        private NeoLoadDataExchangeApiInstance()
+        {
+            IsConnected = false;
+        }
+
+        public static NeoLoadDataExchangeApiInstance GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new NeoLoadDataExchangeApiInstance();
+            }
+            return _instance;
+        }
+
+        public bool IsConnected { get; private set; }
+
+        public bool Connect(string hostname, string port, string key)
+        {
+            if (IsConnected) return true;
+
+            string url = "http://" + hostname + ":" + port + "/DataExchange/v1/Service.svc/";
+            try
+            {
+                _client = DataExchangeAPIClientFactory.NewClient(url, key);
+                IsConnected = true;
+            }
+            catch (NeotysAPIException e) when (e.Message == "NL-DATAEXCHANGE-NO-TEST-RUNNING")
+            {
+                IsConnected = false;
+            }
+
+            return IsConnected;
+        }
+
+        public void AddEntry(string name, double value, string unit, bool passed = true, string message = "")
+        {
+            AddEntry(new List<string> {name}, value, unit, passed, message);
+        }
+
+        public void AddEntry(List<string> path, double value, string unit, bool passed = true, string message = "")
+        {
+            Entry entry = new EntryBuilder(path)
+            {
+                Value = value,
+                Unit = unit,
+
+                Status = new StatusBuilder
+                {
+                    State = passed ? Status.State.Pass : Status.State.Fail,
+                    Message = message
+                }.Build()
+            }.Build();
+
+            new Task(() => { _client.AddEntry(entry); }).Start();
+        }
+
+    }
+}
---
+++ NeoloadTBoxProxy/listener/NeoLoadDataExchangeTestActionListener.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoLoadAddOn.client;
+using Tricentis.Automation.AutomationInstructions.Configuration;
+using Tricentis.Automation.AutomationInstructions.TestActions;
+using Tricentis.Automation.Contract;
+using Tricentis.Automation.Creation;
+using Tricentis.Automation.Engines.Monitoring;
+using Tricentis.Automation.Execution;
+using Tricentis.Automation.Execution.Context;
+using Tricentis.Automation.Execution.Results;
+
+namespace NeoLoadAddOn.listener
+{
+    public class NeoLoadDataExchangeTestActionListener : MonitoringTaskExecutor
+    {
+
+        public NeoLoadDataExchangeTestActionListener(Validator validator) : base(validator)
+        {
+            validator.AssertTrue(() => IsUsingExecutionList && DataExchangeApiEnabled);
+
+            if (validator.IsValid && !NeoLoadDataExchangeApiInstance.GetInstance().IsConnected)
+            {
+                EstablishConnection();
+            }
+        }
+
+        private void EstablishConnection()
+        {
+            if (!MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiHost", out string hostname))
+                hostname = "localhost";
+            if (MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiPort", out string port))
+                port = "7400";
+            if (MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiKey", out string key))
+                key = "";
+            NeoLoadDataExchangeApiInstance.GetInstance().Connect(hostname, port, key);
+        }
+
+        public bool DataExchangeApiEnabled => MainConfiguration.Instance.TryGetBool("UseNeoLoadDataExchangeApi",
+                                                   out bool exchangePerformanceData) && exchangePerformanceData;
+
+        public bool IsUsingExecutionList =>
+            !string.IsNullOrEmpty(RunContext.GetAdditionalExecutionInfo("executionlist.name"));
+
+        public override void PostExecution(ITestAction testAction, ExecutionResult result)
+        {
+            if (NeoLoadDataExchangeApiInstance.GetInstance().IsConnected)
+            {
+                var path = CreatePath();
+                var elapsedMs = (result.EndTime - result.StartTime).TotalMilliseconds;
+                NeoLoadDataExchangeApiInstance.GetInstance().AddEntry(path, elapsedMs, "Milliseconds", result.IsPositive(), result.Message);
+            }
+        }
+
+        public List<string> CreatePath()
+        {
+            List<string> list = new List<string>();
+            var stack = CreatePathRecursive(RunContext.Current, new Stack());
+
+            foreach (var item in stack)
+            {
+                list.Add(item.ToString());
+            }
+
+            return list;
+        }
+
+        private Stack CreatePathRecursive(RunContext parent, Stack path)
+        {
+            if (parent == null)
+            {
+                return path;
+            }
+
+            if (parent.ExecutedItem is AbstractAutomationObject ao) // Folder, TestStep
+            {
+                path.Push(ao.Name);
+            }
+
+            return CreatePathRecursive(parent.Parent, path);
+        }
+    }
+}
