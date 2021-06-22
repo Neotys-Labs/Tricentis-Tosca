b5f7e02fc41083b19b1d638778b6ec1c2267c252
Franz Gasser <f.gasser@tricentis.com>
2021-06-22 13:12:26 +0800

    Added TestCase timing, browser timing, Context support

---
+++ NeoloadTBoxProxy/NeoLoadExtensionHelper.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Tricentis.Automation.Contract;
+using Tricentis.Automation.Engines.Technical.Win32;
+using Tricentis.Automation.Engines.Technicals.Html;
+using Tricentis.Automation.Execution.Context;
+
+namespace NeoLoadAddOn
+{
+    public static class NeoLoadExtensionHelper
+    {
+        public static string GetForeGroundWindowCaption()
+        {
+            int foreGroundWindowId = LocalWin32ObjectManager.EntryPoint.GetForeGroundWindow();
+            IWin32WindowTechnical windowTechnical = LocalWin32ObjectManager.EntryPoint.GetWindow(foreGroundWindowId);
+            return windowTechnical.Caption;
+        }
+
+
+        /// <summary>
+        /// Creates a List of strings of the currently executed AbstractAuomationObject and its parent up to the TestCase level
+        /// Including all folder levels
+        /// </summary>
+        public static List<string> CreatePath()
+        {
+            List<string> path = new List<string>();
+            RunContext parent = RunContext.Current;
+
+            while (parent != null)
+            {
+                if (parent.ExecutedItem is AbstractAutomationObject ao) // TestStep, Folder
+                {
+                    path.Add(ao.Name);
+                }
+
+                parent = parent.Parent;
+            }
+            path.Reverse();
+
+            return path;
+        }
+
+        /// <summary>
+        /// Gets the browsers performance timing information
+        /// </summary>
+        public static Dictionary<string, long> GetBrowserPerformanceTimings(IHtmlEntryPointTechnical htmlEntryPoint)
+        {
+            long redirectStart = 0, fetchStart = 0, responseStart = 0, domContentLoadedEventStart = 0, domLoadEventStart = 0, domLoadEventEnd = 0;
+
+            bool jsSuccess =
+                long.TryParse(htmlEntryPoint.GetJavaScriptResult("return window.performance.timing.redirectStart"), out redirectStart) &&
+                long.TryParse(htmlEntryPoint.GetJavaScriptResult("return window.performance.timing.fetchStart"), out fetchStart) &&
+                long.TryParse(htmlEntryPoint.GetJavaScriptResult("return window.performance.timing.responseStart"), out responseStart) &&
+                long.TryParse(htmlEntryPoint.GetJavaScriptResult("return window.performance.timing.domContentLoadedEventStart"), out domContentLoadedEventStart) &&
+                long.TryParse(htmlEntryPoint.GetJavaScriptResult("return window.performance.timing.loadEventStart"), out domLoadEventStart) &&
+                long.TryParse(htmlEntryPoint.GetJavaScriptResult("return window.performance.timing.loadEventEnd"), out domLoadEventEnd);
+
+            if (!jsSuccess) return null;
+
+            long start = redirectStart == 0 ? fetchStart : redirectStart;
+
+            long timeToFirstByte = responseStart - start;
+            long domContentLoaded = domContentLoadedEventStart - start;
+            long onLoad = domLoadEventStart - start;
+            long documentComplete = domLoadEventEnd - start;
+
+            Dictionary<string, long> performanceDictionary = new Dictionary<string, long>();
+
+            if (timeToFirstByte > 0)
+                performanceDictionary.Add("Time To First Byte", timeToFirstByte);
+            if (domContentLoaded > 0)
+                performanceDictionary.Add("DOM Content Loaded", domContentLoaded);
+            if (onLoad > 0)
+                performanceDictionary.Add("On Load", onLoad);
+            if (documentComplete > 0)
+                performanceDictionary.Add("Document Complete", documentComplete);
+
+            if (documentComplete > 0)
+                performanceDictionary.Add("timestamp", domLoadEventEnd);
+
+            return performanceDictionary;
+
+        }
+    }
+}
--- NeoloadTBoxProxy/listener/NeoLoadDataExchangeTestActionListener.cs
+++ NeoloadTBoxProxy/listener/NeoLoadDataExchangeTestActionListener.cs
@@ -1,46 +1,30 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NeoLoadAddOn.client;
 using Tricentis.Automation.AutomationInstructions.Configuration;
 using Tricentis.Automation.AutomationInstructions.TestActions;
 using Tricentis.Automation.Contract;
 using Tricentis.Automation.Creation;
 using Tricentis.Automation.Engines.Monitoring;
-using Tricentis.Automation.Execution;
+using Tricentis.Automation.Engines.Representations;
+using Tricentis.Automation.Engines.Technicals.Html;
 using Tricentis.Automation.Execution.Context;
+using Tricentis.Automation.Execution.Generic;
 using Tricentis.Automation.Execution.Results;
+using Tricentis.Automation.Helper;
 
 namespace NeoLoadAddOn.listener
 {
     public class NeoLoadDataExchangeTestActionListener : MonitoringTaskExecutor
     {
+        public static bool NewTestStep = false;
+        public static string PreviousBrowserDomain;
+        public static Dictionary<string, long> PreviousBrowserTimings = new Dictionary<string, long>();
 
         public NeoLoadDataExchangeTestActionListener(Validator validator) : base(validator)
         {
             validator.AssertTrue(() => IsUsingExecutionList && DataExchangeApiEnabled);
-
-            if (validator.IsValid && !NeoLoadDataExchangeApiInstance.GetInstance().IsConnected)
-            {
-                EstablishConnection();
-            }
-        }
-
-        /// <summary>
-        /// Establish connection to the DataExchangeApi by extracting the connection details from the test configuration parameters
-        /// </summary>
-        private void EstablishConnection()
-        {
-            if (!MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiHost", out string hostname))
-                hostname = "localhost";
-            if (!MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiPort", out string port))
-                port = "7400";
-            if (!MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiKey", out string key))
-                key = "";
-            NeoLoadDataExchangeApiInstance.GetInstance().Connect(hostname, port, key);
         }
 
         /// <summary>
@@ -56,44 +40,128 @@ namespace NeoLoadAddOn.listener
             !string.IsNullOrEmpty(RunContext.GetAdditionalExecutionInfo("executionlist.name"));
 
         /// <summary>
-        /// Sends the elapsed time after the execution of a testAction via the the DataExchangeApi
+        /// Establish connection to the DataExchangeApi by extracting the connection details from the test configuration parameters
+        /// </summary>
+        protected void EstablishConnection()
+        {
+            if (!MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiHost", out string hostname))
+                hostname = "localhost";
+            if (!MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiPort", out string port))
+                port = "7400";
+            if (!MainConfiguration.Instance.TryGet("NeoLoadDataExchangeApiKey", out string key))
+                key = "";
+
+            string scriptInfo = RunContext.TestCaseName + ", " +
+                             DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffffffK");
+            string softwareInfo = NeoLoadExtensionHelper.GetForeGroundWindowCaption();
+
+            NeoLoadDataExchangeApiInstance.GetInstance().Connect(hostname, port, key, scriptInfo, softwareInfo);
+        }
+
+        /// <summary>
+        /// Only try to connect once at the beginning of the TestCase execution
+        /// </summary>
+        public override void PreExecution()
+        {
+            if (!NeoLoadDataExchangeApiInstance.GetInstance().IsConnected)
+            {
+                EstablishConnection();
+            }
+        }
+
+        /// <summary>
+        /// Called when a new TestStep is executed
+        /// Used afterwards to indicate if the current TestStepValue is the first one inside the TestStep
+        /// </summary>
+        public override void PreExecution(ITestAction testAction)
+        {
+            // Only monitor TestSteps/RootTransactions but not TestStepValues
+            NewTestStep = true;
+        }
+
+        /// <summary>
+        /// Called with every TestStepValue but only used once with the first TestStepValue in a new TestStep
+        /// As Performance metrics don't need to be extracted again with every single TestStepValue
+        /// </summary>
+        public override void PreExecution(IRepresentation context, ITestAction testAction)
+        {
+            if (NewTestStep && NeoLoadDataExchangeApiInstance.GetInstance().IsConnected)
+            {
+                List<string> basePath = NeoLoadExtensionHelper.CreatePath();
+                HandleTechnologySpecificPerformanceTimings(context, basePath);
+                NewTestStep = false;
+            }
+        }
+
+        /// <summary>
+        /// Called when the TestStep execution is finished
+        /// Sends the TestStep timing via the DataExchange Api to NeoLoad
         /// </summary>
         public override void PostExecution(ITestAction testAction, ExecutionResult result)
         {
-            if (NeoLoadDataExchangeApiInstance.GetInstance().IsConnected)
+            // Only monitor TestSteps/RootTransactions not TestStepValues
+            if (NeoLoadDataExchangeApiInstance.GetInstance().IsConnected &&
+                (testAction as AutomationObjectTestAction)?.IsRootAction == true)
             {
-                var path = CreatePath();
-                var elapsedMs = (result.EndTime - result.StartTime).TotalMilliseconds;
-                NeoLoadDataExchangeApiInstance.GetInstance().AddEntry(path, elapsedMs, "Milliseconds", result.IsPositive(), result.Message);
+                double elapsedMs = (result.EndTime - result.StartTime).TotalMilliseconds;
+                List<string> path = NeoLoadExtensionHelper.CreatePath();
+                path.Add("TestStep Duration");
+                NeoLoadDataExchangeApiInstance.GetInstance().SendEntry(path, result.EndTime, elapsedMs, "Ms", result.IsPositive(), result.Message);
             }
         }
 
         /// <summary>
-        /// Creates a List of strings of the currently executed AbstractAuomationObject and its parent up to the TestCase level 
+        /// Called when the TestCase execution is finished
+        /// Sends the TestCase timing via the DataExchange Api to NeoLoad
         /// </summary>
-        public List<string> CreatePath()
+        public override void PostExecution(ExecutionResult result)
         {
-            var list = CreatePathInternal(RunContext.Current, new List<string>());
-            list.Reverse();
-            return list;
+            if (NeoLoadDataExchangeApiInstance.GetInstance().IsConnected &&
+                RunContext.Current?.Executor is IAutomationObjectTestCaseItemExecutor<ExecutionEntry> executor)
+            {
+                List<string> path = new List<string>
+                {
+                    executor.ItemToExecute.Name,
+                    "TestCase Duration"
+                };
+
+                double elapsedMs = (result.EndTime - result.StartTime).TotalMilliseconds;
+                NeoLoadDataExchangeApiInstance.GetInstance().SendEntry(path, result.EndTime, elapsedMs, "Ms", result.IsPositive(), result.Message);
+            }
         }
 
         /// <summary>
-        /// Iterates from the currently executed item up to the test case level
+        /// Handles technology specific performance timings for HTML
         /// </summary>
-        private List<string> CreatePathInternal(RunContext parent, List<string> path)
+        protected virtual void HandleTechnologySpecificPerformanceTimings(IRepresentation context, List<string> basePath)
         {
-            if (parent == null)
+            // Extract js performance timings if the context is a HtmlDocument
+            if (context?.Adapter?.Technical is IHtmlDocumentTechnical htmlTechnical)
             {
-                return path;
+                IHtmlEntryPointTechnical htmlEntryPoint = htmlTechnical.EntryPoint;
+                string htmlBrowserDomain = htmlTechnical.Url;
+                Dictionary<string, long> htmlBrowserTimings = NeoLoadExtensionHelper.GetBrowserPerformanceTimings(htmlEntryPoint);
+
+                // Extract and use the actual domLoadEventEnd timestamp 
+                htmlBrowserTimings.TryGetValue("timestamp", out long timestamp);
+                htmlBrowserTimings.Remove("timestamp");
+
+                // No need to send the values again if they haven't changed
+                if (htmlBrowserDomain != PreviousBrowserDomain || !htmlBrowserTimings.SequenceEqual(PreviousBrowserTimings))
+                {
+                    foreach (KeyValuePair<string, long> timing in htmlBrowserTimings)
+                    {
+                        List<string> timingPath = new List<string>(basePath) { timing.Key };
+                        NeoLoadDataExchangeApiInstance.GetInstance().AddEntryToCache(timingPath, timestamp, timing.Value, "Ms", htmlBrowserDomain);
+                    }
+
+                    PreviousBrowserDomain = htmlBrowserDomain;
+                    PreviousBrowserTimings = htmlBrowserTimings;
+                }
             }
 
-            if (parent.ExecutedItem is AbstractAutomationObject ao) // Folder, TestStep
-            {
-                path.Add(ao.Name);
-            }
-
-            return CreatePathInternal(parent.Parent, path);
+            NeoLoadDataExchangeApiInstance.GetInstance().SendCachedEntries();
         }
+
     }
 }
